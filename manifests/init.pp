4c627f30f71e78c493a21add499d157786dd214d
Jason Lee Miller <jasonlmiller@jlmillerelectric.com>
2023-10-26 21:33:57 -0700

    Adding puppet_client::init with basically all the resources defined for it. I won't be able to test until a few days from now when my new mini pc gets here

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,5 +4,47 @@
 #
 # @example
 #   include puppet_client
-class puppet_client {
+class puppet_client ( String $puppet_server = 'puppet.local' ) {
+
+   exec { 'add puppet-release':
+    command => '/usr/bin/dnf install https://yum.puppetlabs.com/puppet-release-el-8.noarch.rpm',
+    unless => '/usr/bin/dnf list | grep puppet-release 2> /dev/null',
+  }
+
+  package { 'puppet':
+    ensure  => 'present',
+    require => Exec['add puppet-release',]
+  }
+
+  file { '/etc/puppetlabs/':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['puppet'],
+  }
+
+  file { '/etc/puppetlabs/puppet/':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File['/etc/puppetlabs/'],
+  }
+
+  file { '/etc/puppetlabs/puppet/puppet.conf':
+    ensure  => 'present',
+    owner   => 'root'
+    group   => 'root',
+    mode    => '0644',
+    content => template('puppet_client/puppet.conf.erb'),
+    require => File['/etc/puppetlabs/puppet/'],
+    notify  => Service['puppet']
+  }
+
+  service { 'puppet':
+    ensure  => 'running',
+    enable  => true,
+    require => [ Package['puppet'], File['/etc/puppetlabs/puppet/puppet.conf'] ],
+  }
 }
