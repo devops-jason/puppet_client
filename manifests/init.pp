da8a6628b163c5d21ed3e187511499a16fefba4a
Jason Lee Miller <jasonlmiller@jlmillerelectric.com>
2023-10-31 18:51:10 -0700

    refactor init.pp a little bit to support CentOS 8

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,22 +6,28 @@
 #   include puppet_client
 class puppet_client ( String $puppet_server = 'puppet.local' ) {
 
-   exec { 'add puppet-release':
-    command => '/usr/bin/dnf install https://yum.puppetlabs.com/puppet-release-el-8.noarch.rpm',
-    unless  => '/usr/bin/dnf list | grep puppet-release 2> /dev/null',
+  package { 'puppet-release':
+    ensure   => 'installed',
+    source   => 'https://yum.puppetlabs.com/puppet-release-el-8.noarch.rpm',
+    provider => 'rpm',
   }
 
   package { 'puppet':
     ensure  => 'present',
-    require => Exec['add puppet-release',]
+    require => Package['puppet-release'],
+  }
+
+  service { 'puppet':
+    ensure  => 'running',
+    enable  => true,
+    require => [Package['puppet'], File['/etc/puppetlabs/puppet/puppet.conf']],
   }
 
   file { '/etc/puppetlabs/':
-    ensure  => 'directory',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    require => Package['puppet'],
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
 
   file { '/etc/puppetlabs/puppet/':
@@ -33,18 +39,12 @@ class puppet_client ( String $puppet_server = 'puppet.local' ) {
   }
 
   file { '/etc/puppetlabs/puppet/puppet.conf':
-    ensure  => 'present',
+    ensure  => 'file',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => template('puppet_client/puppet.conf.erb'),
     require => File['/etc/puppetlabs/puppet/'],
-    notify  => Service['puppet']
-  }
-
-  service { 'puppet':
-    ensure  => 'running',
-    enable  => true,
-    require => [ Package['puppet'], File['/etc/puppetlabs/puppet/puppet.conf'] ],
+    notify  => Service['puppet'],
   }
 }
